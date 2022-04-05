1ffecc38d7bb3ea0d19e4b2fd69b61ff59c957d3
Arshad <arshad.syed.anwar@gmail.com>
2022-04-05 23:42:01 +0600

    added 404

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 // import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home/Home';
-import Reviews from './components/Reviews/Reviews';
-import Dashboard from './components/Dashboard/Dashboard';
-import Blogs from './components/Blogs/Blogs';
-import About from './components/About/About';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
+import "./App.css";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import FourZeroFour from "./components/FourZeroFour/FourZeroFour";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
-        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*"  element={<FourZeroFour></FourZeroFour>}>
+        </Route>
       </Routes>
-      
     </div>
   );
 }
---
+++ src/components/FourZeroFour/FourZeroFour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FourZeroFour = () => {
+  return (
+    <div>
+      <h1>The page your are looking for could not be found</h1>
+      <h1>Error Code: 404</h1>
+    </div>
+  );
+};
+
+export default FourZeroFour;
