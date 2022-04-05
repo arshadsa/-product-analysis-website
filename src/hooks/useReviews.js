1dc31daf6d3a8a58e67c028991745db7891f0941
Arshad <arshad.syed.anwar@gmail.com>
2022-04-05 23:50:00 +0600

    all done

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -2,7 +2,9 @@ const { useState, useEffect } = require("react")
 
 const useReviews = () =>{
   const [reviews, setReviews] = useState([]);
-
+  useEffect(() => {
+    document.title = "Great Shoe"
+ }, []);
   useEffect(() => {
     fetch('reviews.json')
     .then(res => res.json())
